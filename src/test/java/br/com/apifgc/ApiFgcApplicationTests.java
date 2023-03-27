94e014e0295c2c61c5b2b1cf737092f0cc59b993
Weverton-profile <wevertonalves.profile@gmail.com>
2023-03-27 19:02:11 -0300

    initial commit

---
+++ src/test/java/br/com/apifgc/ApiFgcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apifgc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
